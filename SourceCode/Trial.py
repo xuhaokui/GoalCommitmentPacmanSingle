a581da20a6a771c64e0d677ae49f30abb3880ec5
CathyAn01 <aw93126@126.com>
2018-12-20 11:30:47 +0800

    Update Trial.py
    更改了处理时间的包，改用pygame.time
    放弃更新每一步的反应时与路径，相应删掉results里的这两条信息，改为记录第一次反应时和全程反应时 
    把第一次反应的语句提到原来的while外面，加了一个while是因为需要判断被试是否做了第一次按键反应
    改掉了results的记录 错误（索引）
    删掉了trial初始化里的counter和updateWorld
    增加了第49行语句，否则trial之间被试冗余的按键会影响下一个trial的playerGrid的初始位置

--- SourceCode/Trial.py
+++ SourceCode/Trial.py
@@ -3,16 +3,14 @@ from Visualization import DrawBackground,DrawNewState,DrawImage
 from Controller import HumanController
 import UpdateWorld
 import pygame as pg
-import time
 import collections as co
+from pygame import time
 
 
 class Trial():
-	def __init__(self,updateWorld,humanController,drawNewState):
-		self.updateWorld=updateWorld
+	def __init__(self,humanController,drawNewState):
 		self.humanController=humanController
 		self.drawNewState=drawNewState
-		self.counter=0
 
 	def checkEaten(self,bean1Grid, bean2Grid, humanGrid):
 		if np.linalg.norm(np.array(humanGrid) - np.array(bean1Grid), ord=1)==0:
@@ -30,44 +28,36 @@ class Trial():
 			pause=True
 		return pause
 
-	def updateTrajectory(self, action, trajectoryList, playerPosition):
-		if action in self.humanController.actionDict.values():
-			trajectoryList.append(playerPosition)
-		return trajectoryList
-
-	def updateResponseTime(self, action, responseTime):
-		if action in self.humanController.actionDict.values():
-			now = time.time()
-			responseTime.append(now)
-		return responseTime
-
 	def __call__(self,bean1Grid,bean2Grid,playerGrid):
 		pause=True
-		self.counter=self.counter+1
-		initialTime = time.time()
-		responseTimeList=[initialTime]
-		trajectoryList=[]
+		initialTime = time.get_ticks()
 		results=co.OrderedDict()
-		eatenFlag=[False,False]
 		pg.event.set_allowed([pg.KEYDOWN, pg.KEYUP])
+		firstActionFlag=False
+		while not firstActionFlag:
+			self.drawNewState(bean1Grid, bean2Grid, playerGrid)
+			playerGrid, action = self.humanController(playerGrid)
+			eatenFlag = self.checkEaten(bean1Grid, bean2Grid, playerGrid)
+			if action in self.humanController.actionDict.values():
+				firstResponseTime=time.get_ticks()-initialTime
+				firstActionFlag=True
 		while pause:
 			self.drawNewState(bean1Grid, bean2Grid, playerGrid)
 			playerGrid,action=self.humanController(playerGrid)
-			trajectoryList = self.updateTrajectory(action, trajectoryList, playerGrid)
-			responseTimeList = self.updateResponseTime(action, responseTimeList)
 			eatenFlag=self.checkEaten(bean1Grid, bean2Grid,playerGrid)
 			pause=self.checkTerminationOfTrial(action,eatenFlag)
-
+		pg.event.set_blocked([pg.KEYDOWN, pg.KEYUP])
+		wholeResponseTime=time.get_ticks()-initialTime
 		self.drawNewState(bean1Grid, bean2Grid, playerGrid)
 		results["bean1GridX"] = bean1Grid[0]
 		results["bean1GridY"] = bean1Grid[1]
 		results["bean2GridX"] = bean2Grid[0]
 		results["bean2GridY"] = bean2Grid[1]
 		results["playerGridX"] = playerGrid[0]
-		results["playerGridY"] = playerGrid[0]
-		if (True in eatenFlag):results["beanEaten"] = eatenFlag.index(True)
-		results["responseTime"]=str(responseTimeList)
-		results["trajectory"]=str(trajectoryList)
+		results["playerGridY"] = playerGrid[1]
+		if (True in eatenFlag):results["beanEaten"] = eatenFlag.index(True)+1
+		results["firstResponseTime"]=firstResponseTime
+		results["trialTime"]=wholeResponseTime
 		return results,action
 
 
@@ -79,9 +69,7 @@ def main():
 	bounds = [0, 0, dimension - 1, dimension - 1]
 	minDistanceBetweenGrids = 5
 	condition = [-5, -3, -1, 0, 1, 3, 5]
-	counter = [0] * len(condition)
 	initialWorld = UpdateWorld.InitialWorld(bounds)
-	updateWorld = UpdateWorld.UpdateWorld(bounds, condition, counter, minDistanceBetweenGrids)
 	pg.init()
 	screenWidth = 720
 	screenHeight = 720
@@ -98,7 +86,7 @@ def main():
 	drawBackground = DrawBackground(screen, gridSize, leaveEdgeSpace, backgroundColor, lineColor, lineWidth)
 	drawNewState = DrawNewState(screen, drawBackground, targetColor, playerColor, targetRadius, playerRadius)
 	humanController=HumanController(gridSize)
-	trial=Trial(updateWorld,humanController,drawNewState)
+	trial=Trial(humanController,drawNewState)
 	bean1Grid,bean2Grid,playerGrid=initialWorld(minDistanceBetweenGrids)
 	results=trial(bean1Grid,bean2Grid,playerGrid)
 	print(results)
