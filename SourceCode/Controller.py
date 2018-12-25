b70a132014913f072499245e895b173491280ad0
wei an <aw93126@126.com>
2018-12-26 00:51:59 +0800

    delete the pycharm hints

---
+++ SourceCode/Controller.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pygame as pg
+import Visualization
+
+class HumanController():
+	def __init__(self,gridSize,stopwatchEvent,stopwatchUnit,drawNewState):
+		self.actionDict={pg.K_UP:[0,-1], pg.K_DOWN:[0,1], pg.K_LEFT:[-1,0], pg.K_RIGHT:[1,0]}
+		self.gridSize=gridSize
+		self.stopwatchEvent=stopwatchEvent
+		self.stopwatchUnit=stopwatchUnit
+		self.stopwatch=0
+		self.drawNewState=drawNewState
+	def __call__(self,targetPositionA,targetPositionB,playerPosition,currentScore,currentStopwatch):
+		pause=True
+		playerNextPosition=playerPosition.copy()
+		while pause:
+			# pg.time.wait(10)
+			self.drawNewState(targetPositionA,targetPositionB,playerNextPosition,currentStopwatch,currentScore)
+			for event in pg.event.get():
+				if event.type == pg.KEYDOWN and event.key in self.actionDict.keys() and np.all(np.add(playerPosition,self.actionDict[event.key])>=0) and np.all(np.add(playerPosition,self.actionDict[event.key])<self.gridSize):
+					pause=False
+					action = self.actionDict[event.key]
+					playerNextPosition = np.add(playerPosition,action)
+					self.drawNewState(targetPositionA,targetPositionB,playerNextPosition,currentStopwatch,currentScore)
+				elif event.type == pg.QUIT:
+					pause=False
+					action=pg.QUIT
+					playerNextPosition = playerPosition.copy()
+				if event.type == self.stopwatchEvent:
+					currentStopwatch=currentStopwatch+self.stopwatchUnit
+		# if np.any(playerNextPosition<0) or np.any(playerNextPosition>=self.gridSize):
+		# 	playerNextPosition = playerPosition.copy()
+			pg.display.flip()
+		return playerNextPosition,action,currentStopwatch
+
+class ModelController():
+	def __init__(self):
+		pass
+	def __call__(self):
+		return
+
+if __name__=="__main__":
+	pg.init()
+	screenWidth=720
+	screenHeight=720
+	screen=pg.display.set_mode((screenWidth,screenHeight))
+	gridSize=20
+	leaveEdgeSpace=2
+	lineWidth=2
+	backgroundColor=[188,188,0]
+	lineColor=[255,255,255]
+	targetColor=[255,50,50]
+	playerColor=[50,50,255]
+	targetRadius=10
+	playerRadius=10
+	targetPositionA=[5,5]
+	targetPositionB=[15,5]
+	playerPosition=[10,15]
+	currentScore=5
+	textColorTuple=(255,50,50)
+	stopwatchEvent = pg.USEREVENT + 1
+	stopwatchUnit=10
+	pg.time.set_timer(stopwatchEvent, stopwatchUnit)
+
+	drawBackground=Visualization.DrawBackground(screen, gridSize, leaveEdgeSpace, backgroundColor, lineColor, lineWidth, textColorTuple)
+	drawNewState=Visualization.DrawNewState(screen, drawBackground, targetColor, playerColor, targetRadius, playerRadius)
+
+	getHumanAction = HumanController(gridSize, stopwatchEvent, stopwatchUnit, drawNewState)
+
+	[playerNextPosition,action,stopwatch]=getHumanAction(targetPositionA, targetPositionB, playerPosition, currentScore)
+	print(playerNextPosition,action,stopwatch)
---
+++ SourceCode/Writer.py
@@ -0,0 +1,12 @@
+
+import pandas as pd 
+import os 
+
+class WriteDataFrameToCSV():
+	def __init__(self,saveResultFile):
+		self.saveResultFile=saveResultFile
+	def __call__(self,responseDF):
+		if not os.path.isfile(self.saveResultFile):
+			responseDF.to_csv(self.saveResultFile, header=list(responseDF.columns))
+		else:
+			responseDF.to_csv(self.saveResultFile, mode='a', header=False)
